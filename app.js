ff1935f03c59107b4244e7f7d1c0c75442c9e08e
Laeticiaa <nathoo.laeticia@gmail.com>
2017-02-03 13:32:31 +0100

    Start TP3

--- app.js
+++ app.js
@@ -1,25 +1,50 @@
 //importing modules
 var express = require( 'express' );
 var request = require( 'request' );
-var cheerio = require( 'cheerio' );
-
+var cheerio = require( 'cheerio' )
+var port = 4000;
+var MyData;
 //creating a new express server
 var app = express();
 
-//setting EJS as the templating engine
-app.set( 'view engine', 'ejs' );
+//creating a var for the url
+var url = "https://www.leboncoin.fr/ventes_immobilieres/942365902.htm?ca=12_s"
 
-//setting the 'assets' directory as our static assets dir (css, js, img, etc...)
-app.use( '/assets', express.static( 'assets' ) );
+request( url, function ( err, resp, body ) {
+    var $ = cheerio.load( body );
+    var ville = $( '*[itemprop = "address"]' );
+    var villeText = ville.text().trim();
 
+    villeTextsplit = villeText.split( " " );
+    var PostalCode = villeTextsplit[villeTextsplit.length - 1];
+
+    var prix = $( '.item_price' ).attr( "content" );
+
+    var surface = $( '#adview > section > section > section.properties.lineNegative > div:nth-child(9) > h2 > span.value' );
+    var surfaceText = surface.text().split( " " );
+
+    var typeText = $( '#adview > section > section > section.properties.lineNegative > div:nth-child(7) > h2 > span.value' ).text();
+
+    MyData = {
+        type: typeText,
+        city: villeText,
+        postal_code: PostalCode,
+        price: prix,
+        surface: surfaceText[0],
+    }
+    console.log( MyData );
+
+
+})
+app.listen( port );
+console.log( 'server is Listening on port' + port );
 
 //makes the server respond to the '/' route and serving the 'home.ejs' template in the 'views' directory
 app.get( '/', function ( req, res ) {
     res.render( 'home', {
-        message: 'The Home Page!'
+        message: MyData
     });
 });
-
 
 //launch the server on the 3000 port
 app.listen( 3000, function () {
